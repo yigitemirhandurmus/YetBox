0d1c996161c74ca22acb5f556c9f3196637ed3ed
yigitemirhandurmus <45107558+yigitemirhandurmus@users.noreply.github.com>
2024-08-01 22:28:11 +0300

    Add function to recommend random films

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 import os
 import csv
+import random
+
+with open("films.csv", "r", encoding="cp1252") as file:
+    reader = csv.reader(file, delimiter=',')
+    lines = list(reader)
+
+print("""
+YetBox'a Hoşgeldiniz!
+
+1. Bana rastgele bir film öner
+      
+Çıkmak için q tuşuna basın.
+      """)
+
+class yetbox():
+    
+    def randomFilm():
+        randomSelection = random.choice(lines[1:])
+        print(f"""
+İsim: {randomSelection[0]}
+Tür: {randomSelection[1]}
+Çıkış Tarihi: {randomSelection[2]}
+Süre: {randomSelection[3]}
+IMDB Puanı: {randomSelection[4]}
+Dil: {randomSelection[5]}
+              """)
+
+while True:
+    userSelection = input("Seçiminizi yapın: ")
+    if userSelection == "1":
+        yetbox.randomFilm()
+    elif userSelection.lower() == "q":
+        break

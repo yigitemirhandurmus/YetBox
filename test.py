6ee0c14b1d0ee69f95c8e6b98a1b37a66219d11a
yigitemirhandurmus <45107558+yigitemirhandurmus@users.noreply.github.com>
2024-08-12 00:14:21 +0300

    Update the account creation & login system
    The work still has to be integrated into the main.py though

--- test.py
+++ test.py
@@ -1,38 +1,63 @@
-def sign_up():
-    global user_name
-    global user_surname
-    global user_gmail
-    global user_password
-    global user_password_again
-    global nickname
-    user_name = input("Name ")
-    user_surname = input("Surname ")
-    user_gmail = input("Gmail ")
-    nickname = input("Nickname ")
-    user_password = input("Password (0-9,a-z,A-Z,special characters) ")
-    user_password_again = input("Password (0-9,a-z,A-Z,special characters) ")
-    if user_password_again == user_password:
-        return ("Hesabınız oluşturuldu.")
-    else:
-        print("Şifreleriniz uyumsuz!")
-        while True:
-            user_password_again = input("Password (0-9,a-z,A-Z,special characters) ")
-            if user_password_again == user_password:
-                print("Hesabınız oluşturuldu.")
-                break
-sign_up()
+import os
+import csv
 
-def login():
-    control_nickname = input("Nickname ")
-    control_password = input("Password (0-9,a-z,A-Z,special characters) ")
-    if control_password != user_password:
-        for entry in range(5):
-            control_password = input("Password (0-9,a-z,A-Z,special characters) ")
-            if control_password == user_password:
-                print("Başarıyla giriş yapıldı.")
-                break
-            else:
-                print(f"{4 - entry} giriş hakkın kaldı!")
-    else:
-        return ("Başarıyla giriş yapıldı.")
-login()
+if not os.path.exists("users.csv"):
+    with open("users.csv", "w", encoding="utf-8") as loginFile:
+        pass
+
+userLoggedIn = False
+class userManagement():
+    def sign_up():
+        global user_name
+        global user_surname
+        global user_email
+        global user_password
+        global user_password_again
+        global nickname
+        user_name = input("İsim: ")
+        user_surname = input("Soyisim: ")
+        user_email = input("E-mail: ")
+        nickname = input("Kullanıcı adı: ")
+        user_password = input("Şifre: ")
+        user_password_again = input("Şifrenizi tekrar girin: ")
+        if user_password_again == user_password:
+            with open("users.csv", "a", encoding="utf-8") as loginFile:
+                writer = csv.writer(loginFile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([nickname, user_name, user_surname, user_email, user_password])
+            print("Hesabınız oluşturuldu.")
+        else:
+            print("Şifreleriniz uyumsuz! Tekrar deneyin.")
+
+    def login():
+        global userLoggedIn
+        control_nickname = input("Kullanıcı adınızı girin: ")
+        control_password = input("Şifrenizi girin: ")
+        with open("users.csv", "r", encoding="utf-8") as loginFile:
+            reader = csv.reader(loginFile)
+            credentials = list(reader)
+            print(credentials)
+
+            for i in credentials:
+                if control_nickname == i[0] and control_password == i[4]:
+                    user = str(i[0])
+                    print(f"Başarıyla giriş yaptınız! Kullanıcı adınız: {i[0]}")
+                    userLoggedIn = True
+            if userLoggedIn == False:
+                print("Kullanıcı bulunamadı!")
+        
+print("""Hoşgeldiniz.
+Yapmak istediğiniz eylemi seçin:
+      1. Kullanıcı girişi
+      2. Yeni kullanıcı oluşturma
+
+      q. Çıkış      
+""")
+
+while True:
+    loginSelection = input("Seçiminizi yapın: ")
+    if loginSelection == "1":
+        userManagement.sign_up()
+    if loginSelection == "2":
+        userManagement.login()
+    if loginSelection == "q":
+        break
